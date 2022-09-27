91cc745307185ed988173e1baac1787f7f89b0af
mr.ikanx <54991077+ikanx101@users.noreply.github.com>
2022-09-27 13:52:58 +0700

    Create Install r base debian.sh

---
+++ Install r base debian.sh
@@ -0,0 +1,6 @@
+apt install pandoc gnupg ca-certificates nano gdebi-core build-essential libgdal-dev
+apt install libcurl4-openssl-dev libssl-dev libxml2-dev cmake
+
+apt-get update
+apt-get upgrade
+apt-get install r-base r-base-dev
