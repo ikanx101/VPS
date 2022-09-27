94102920bb2c7b1a4a9f3c09f6d56c5ae9d1fda3
mr.ikanx <54991077+ikanx101@users.noreply.github.com>
2022-09-27 19:25:41 +0700

    Update installer.R

--- installer.R
+++ installer.R
@@ -3,7 +3,7 @@ rm(list=ls())
 pakets = installed.packages()
 pakets_needed = c("dplyr","tidyr","readxl","janitor","openxlsx","stringr",
                   "rvest","ggplot2","txtplot","tidytext","reshape2",
-                  "readxl","qpdf","ggpubr","ggrepel")
+                  "readxl","qpdf","ggpubr","ggrepel","ROI","ompr","ompr.roi","ROI.plugin.glpk")
 necessary = setdiff(pakets_needed,pakets)
 if(length(necessary) > 0){
   for(i in 1:length(necessary)) install.packages(necessary[i])
