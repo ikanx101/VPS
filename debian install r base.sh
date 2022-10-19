853105066dbb3fd40d2eb335b578731c8feaafe6
mr.ikanx <54991077+ikanx101@users.noreply.github.com>
2022-10-19 16:16:25 +0700

    Update debian install r base.sh

--- debian install r base.sh
+++ debian install r base.sh
@@ -1,5 +1,6 @@
-apt install pandoc gnupg ca-certificates nano gdebi-core build-essential libgdal-dev
-apt install libcurl4-openssl-dev libssl-dev libxml2-dev cmake libglpk-dev
+apt install pandoc gnupg ca-certificates nano gdebi-core build-essential libgdal-dev python3-pip
+apt install libcurl4-openssl-dev libssl-dev libxml2-dev cmake libglpk-dev libcairo2-dev libfontconfig1-dev
+pip3 install -U radian
 
 apt-get update
 apt-get upgrade
