5faa302f98c46f12a61f1dd51d39059d397f8386
mr.ikanx <54991077+ikanx101@users.noreply.github.com>
2022-11-15 09:56:19 +0700

    tambahin Selenium

--- R/install paket.R
+++ R/install paket.R
@@ -6,7 +6,8 @@ pakets_needed = c("dplyr","tidyr","readxl","janitor","openxlsx","stringr",
                   "rvest","ggplot2","txtplot","tidytext","reshape2",
                   "readxl","qpdf","ggpubr","ggrepel","ROI","ompr","ompr.roi",
                   "ROI.plugin.glpk","rmarkdown","Ryacas","languageserver","httpgd",
-                  "forecast","TTR","tseries","fpp","TSstudio","padr","MLmetrics")
+                  "forecast","TTR","tseries","fpp","TSstudio","padr","MLmetrics",
+                  "RSelenium")
 necessary = setdiff(pakets_needed,pakets)
 if(length(necessary) > 0){
   for(i in 1:length(necessary)) install.packages(necessary[i])
