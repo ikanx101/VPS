86a73b3b55096e505b24365eada4999adf41d30b
mr.ikanx <54991077+ikanx101@users.noreply.github.com>
2022-12-28 11:56:39 +0700

    Update install paket.R

--- R/install paket.R
+++ R/install paket.R
@@ -7,7 +7,7 @@ pakets_needed = c("dplyr","tidyr","readxl","janitor","openxlsx","stringr",
                   "readxl","qpdf","ggpubr","ggrepel","ROI","ompr","ompr.roi",
                   "ROI.plugin.glpk","rmarkdown","Ryacas","languageserver","httpgd",
                   "forecast","TTR","tseries","fpp","TSstudio","padr","MLmetrics",
-                  "RSelenium","gganimate","av","multidplyr")
+                  "RSelenium","gganimate","av","multidplyr","telegram.bot","telegram")
 necessary = setdiff(pakets_needed,pakets)
 if(length(necessary) > 0){
   for(i in 1:length(necessary)) install.packages(necessary[i])
